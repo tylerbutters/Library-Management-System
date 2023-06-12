efcf8c02d3d2429c9f9eb0e4e2667cf69d83bb6f
Tom Morrison <thomas.m.morrison.101@gmail.com>
2023-06-12 16:29:42 +1200

    Login system condensed into method

--- LMS/Pages/Login.xaml.cs
+++ LMS/Pages/Login.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,62 +15,61 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
+
 namespace LMS.Pages
 {
-    /// <summary>
-    /// Interaction logic for Login.xaml
-    /// </summary>
     public partial class Login : Page
     {
+        const string userLink = @".\Database\UserPasswords.csv";
+        const string adminLink = @".\Database\AdminPasswords.csv";
         public delegate void NavigateTo_Member_Homepage(object sender, RoutedEventArgs e);
         public event NavigateTo_Member_Homepage Navigateto_member_homepage;
         public delegate void NavigateTo_Admin_Homepage(object sender, RoutedEventArgs e);
         public event NavigateTo_Admin_Homepage Navigateto_admin_homepage;
+        //Login Check checks the entered email and password against the given list and returns true/false.
         public Login()
         {
             InitializeComponent();
         }
-
-        
-        //Login Button checks database and then runs NavigateTo_Member_Homepage if login successful
-        private void LogIn_Button_Click(object sender, RoutedEventArgs e)
+        public bool Login_Check(string databaseLink)
         {
-            //use if/else loop to check admin CSV first, else member CSV
-
             string inputEmail = LoginEmail.Text;
             string inputPassword = LoginPassword.Text;
-            string[] lines = File.ReadAllLines(@".\Database\UserPasswords.csv");
+            string[] lines = File.ReadAllLines(databaseLink);
             var UserNameData = lines.Skip(1).Select(row => row.Split(',')[0]).ToList();
             var PasswordData = lines.Skip(1).Select(row => row.Split(',')[1]).ToList();
-
             if (inputEmail == "" || inputPassword == "")
             {
                 MessageBox.Show("Please enter both username and password");
-                return;
+                return false;
             }
             else if (UserNameData.Contains(inputEmail) && PasswordData.Contains(inputPassword))
             {
-                
-                Navigateto_member_homepage(sender, e);
-                return;
+                return true;
             }
             else
             {
-
-                string[] adminlines = File.ReadAllLines(@".\Database\AdminPasswords.csv");
-                var adminUserNameData = adminlines.Skip(1).Select(row => row.Split(',')[0]).ToList();
-                var adminPasswordData = adminlines.Skip(1).Select(row => row.Split(',')[1]).ToList();
-
-                if (adminUserNameData.Contains(inputEmail) && PasswordData.Contains(inputPassword))
-                {
-                    MessageBox.Show("Logging in as Admin");
-                    Navigateto_admin_homepage(sender, e);
-                    return;
-                }
-                else
-                {
-                    MessageBox.Show("Please enter a valid username and password");
-                }
+                return false;
+            }
+        }
+        //Login button runs "Login_Check()" against user list first, then admin list.
+        private void LogIn_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Login_Check(userLink))
+            {
+                Navigateto_member_homepage(sender, e);
+            }
+            else if (Login_Check(adminLink))
+            {
+                MessageBox.Show("Logging in as Admin");
+                Navigateto_admin_homepage(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Username/Password incorrect");
+                return;
             }
         }
     }
