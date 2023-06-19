3ed7ad51a063b413a6a47c92550a4abf53fe0614
Tyler Butters <johntylerbutters@gmail.com>
2023-06-19 16:36:57 +1200

    merged tom add image

--- LMS/Pages/AdminPages/AdminBookPages/AddBookPage.xaml.cs
+++ LMS/Pages/AdminPages/AdminBookPages/AddBookPage.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.Win32;
+using System.Runtime.CompilerServices;
 
 namespace LMS.Pages.AdminPages
 {
@@ -23,6 +27,7 @@ namespace LMS.Pages.AdminPages
     {
         public delegate void NavigateToBookPage(object sender, RoutedEventArgs e);
         public event NavigateToBookPage navigateToBookPage;
+        public string selectedFile = null;
         public AddBookPage()
         {
             InitializeComponent();
@@ -33,6 +38,52 @@ namespace LMS.Pages.AdminPages
             int max = 99999;
             return new Random().Next(min, max);
         }
+        
+        private void AddBookPageScrollChanged(object sender, ScrollChangedEventArgs e)
+        { }
+        public string GenerateNewImageAddress(string title, string existingImageAddressInput)
+        {
+            string imageDirectory = @"Database\CoverImages\";                                                                                                                                       //Folder to contain new Image
+            string cleanedExistingAddressInput = string.Join("_", System.IO.Path.GetInvalidPathChars().Aggregate(existingImageAddressInput, (current, c) => current.Replace(c.ToString(), "")));    //Removes illegal path characters.
+            string fileExtension = System.IO.Path.GetExtension(cleanedExistingAddressInput);                                                                                      //gets file extension of existing image       
+            //string imageExtension = fileExtension.TrimStart('\\');
+            string cleanedTitle = string.Join("_", System.IO.Path.GetInvalidFileNameChars().Aggregate(title, (current, c) => current.Replace(c.ToString(), ""))).Replace(" ", "_");                  //Removes illegal filename characters from book title.                                                                                                                                                                                        //makes an address and name for the new copy, preserves existing filetype. (does NOT save a copy yet)
+            string newImageAddress = $"{imageDirectory}{cleanedTitle}{fileExtension}";
+            return newImageAddress;
+        }
+
+        //Select Image File button handler
+        public void SelectImageButtonClick(object sender, RoutedEventArgs e)
+        {
+            selectedFile = SelectImageDialog();
+
+        }
+
+        public string SelectImageDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "All Files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Title = "Select a cover image";
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                MessageBox.Show("Image Selected");
+                string selectedFile = openFileDialog.FileName;
+                selectedImageAddress.Text = selectedFile;
+                return selectedFile;
+            }
+            else
+            {
+                MessageBox.Show("No Image Selected");
+                return null;
+            }
+
+        }
+
         private void SaveNewBookButtonClick(object sender, RoutedEventArgs e)
         {
             //only filters in accounts of type Book
@@ -44,9 +95,13 @@ namespace LMS.Pages.AdminPages
                 return;
             }
 
+            string newImageAddress = GenerateNewImageAddress(titleInput.Text, selectedFile); //creates new image address and filename
+            File.Copy(selectedFile, newImageAddress);
+
             Book newBook = new Book
             {
                 id = GenerateRandomID().ToString(),
+                cover = newImageAddress,
                 title = titleInput.Text,
                 authorFirstName = authorFirstNameInput.Text,
                 authorLastName = authorLastNameInput.Text,
@@ -75,6 +130,7 @@ namespace LMS.Pages.AdminPages
             authorLastNameInput.Text = "";
             tagInput.Text = "";
             summaryInput.Text = "";
+            selectedImageAddress.Text = "";
         }
         private void BackButtonClick(object sender, RoutedEventArgs e)
         {
