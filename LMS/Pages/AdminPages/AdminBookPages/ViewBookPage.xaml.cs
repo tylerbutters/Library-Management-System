181cd71f46890f08c0474dcf8c53149aa8e9457a
Thomas Morrison <270010732@yoobeestudent.ac.nz>
2023-06-23 15:11:42 +1200

    fixed images

--- LMS/Pages/AdminPages/AdminBookPages/ViewBookPage.xaml.cs
+++ LMS/Pages/AdminPages/AdminBookPages/ViewBookPage.xaml.cs
@@ -40,7 +40,7 @@ namespace LMS.Pages.AdminPages
             Subject.Text = book.subject;
             Summary.Text = book.summary;
             Cover.Text = book.cover;
-
+            imageAddress = book.cover;
         }
         private void BackButtonClick(object sender, RoutedEventArgs e)
         {
@@ -115,11 +115,11 @@ namespace LMS.Pages.AdminPages
                 Cover.Text = book.cover;
             }
         }
-        
+       
         private void SelectImageButtonClick(object sender, RoutedEventArgs e)
         {
             imageAddress = SelectImageDialog();
-            CoverImage.Source = new BitmapImage(new Uri(imageAddress));
+            CoverImage.Source = new BitmapImage(new Uri(imageAddress, UriKind.Relative));
             
 
         }
@@ -149,19 +149,23 @@ namespace LMS.Pages.AdminPages
         }
         private string GenerateNewImageAddress(string title, string existingImageAddressInput)
         {
-            string folderPath = @"/CoverImages/";//Folder to contain new Image
+            string folderPath = @"C:\Users\270010732\source\repos\106-LMS\LMS\CoverImages\";//Folder to contain new Image
             string cleanedExistingAddressInput = string.Join("_", System.IO.Path.GetInvalidPathChars().Aggregate(existingImageAddressInput, (current, c) => current.Replace(c.ToString(), "")));//Removes illegal path characters.
             string fileExtension = System.IO.Path.GetExtension(cleanedExistingAddressInput);//gets file extension of existing image       
             string cleanedTitle = string.Join("_", System.IO.Path.GetInvalidFileNameChars().Aggregate(title, (current, c) => current.Replace(c.ToString(), ""))).Replace(" ", "_");//Removes illegal filename characters from book title.                                                                                                                                                                                        //makes an address and name for the new copy, preserves existing filetype. (does NOT save a copy yet)
 
+            MessageBox.Show($"{folderPath}{cleanedTitle}{fileExtension}");
             return $"{folderPath}{cleanedTitle}{fileExtension}";
         }
         private void SaveButtonClick(object sender, RoutedEventArgs e)
         {
             Book changedInfo = new Book { id = ID.Text, cover = book.cover, title = Title.Text, authorFirstName = AuthorFirstName.Text, authorLastName = AuthorLastName.Text, subject = Subject.Text, summary = Summary.Text };
-
-            string newimageAddress = GenerateNewImageAddress(Title.Text, imageAddress);
-            File.Copy(imageAddress, newimageAddress, true);
+            if (imageAddress != book.cover)
+            {
+                string newimageAddress = GenerateNewImageAddress(Title.Text, imageAddress);
+                MessageBox.Show(newimageAddress);
+                File.Copy(imageAddress, newimageAddress, true);
+            }
             //if (File.Exists(imageAddress))
             //{
             //    if (imageAddress != book.cover)
